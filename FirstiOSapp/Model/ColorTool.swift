00f1cc20330019d1a372738f22277dcad6a516c9
Jenna Mills <superangelfish555@gmail.com>
2018-10-09 08:44:34 -0600

    Created ColorTool model

---
+++ FirstiOSapp/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  FirstiOSapp
+//
+//  Created by Mills, Jenna on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
